e10bbc2508a282e116696ef5a996f56072b6a15a
mdmaruf641 <mdmarufmoni50@gmail.com>
2021-07-31 22:45:16 +0600

    all sections completed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,49 @@
 $(function(){
+        //=====SKICLY MENU=====//
+
+    var headerPosition = $('.main-nav').offset().top;
+    $(window).scroll(function(){
+    var scrollValue = $(window).scrollTop();
+    if(scrollValue > headerPosition) {
+        $('.main-nav').addClass('sticky')
+    }
+    else{
+        $('.main-nav').removeClass('sticky');
+    }
+    });
+
+    
+      //=====bottom to top button=====//
+
+      $(window).scroll(function(){
+
+        var scrolling = $(this).scrollTop();
+
+        if(scrolling > 50){
+            $(".bottom-to-top").fadeIn(500);
+        }
+        else{
+            $(".bottom-to-top").fadeOut(500); 
+        }
+        // sticky menu //
+        if(scrolling > 100){
+            $("#nav").addClass("sticky-bg");
+        }
+        else{
+            $("#nav").removeClass("sticky-bg");
+        }
+      });
+
+     $(".bottom-to-top").click(function(){
+        $('html, body').animate({
+            scrollTop:0
+        }, 2000);
+     });      
+    // PRE LOADER //
+
+    $(window).on("load", function(){
+        $("#loading").delay(500) .fadeOut(2000)
+    });
     // mixitup
     var mixer = mixitup('.mix-img');
    
@@ -7,4 +52,6 @@ $(function(){
         'resizeDuration': 200,
         'wrapAround': true,
     });
+
+
 });
