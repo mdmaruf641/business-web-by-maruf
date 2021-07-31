a7a3075fc50737fccbd2132b6b07699ca49e14d3
mdmaruf641 <mdmarufmoni50@gmail.com>
2021-07-31 23:30:55 +0600

    final touch

--- js/main.js
+++ js/main.js
@@ -1,57 +1,49 @@
-$(function(){
-        //=====SKICLY MENU=====//
+$(function () {
 
+    //=====SKICLY MENU=====//
     var headerPosition = $('.main-nav').offset().top;
-    $(window).scroll(function(){
-    var scrollValue = $(window).scrollTop();
-    if(scrollValue > headerPosition) {
-        $('.main-nav').addClass('sticky')
-    }
-    else{
-        $('.main-nav').removeClass('sticky');
-    }
+    $(window).scroll(function () {
+        var scrollValue = $(window).scrollTop();
+        if (scrollValue > headerPosition) {
+            $('.main-nav').addClass('sticky')
+        } else {
+            $('.main-nav').removeClass('sticky');
+        }
     });
 
-    
-      //=====bottom to top button=====//
 
-      $(window).scroll(function(){
+    //=====bottom to top button=====//
+
+    $(window).scroll(function () {
 
         var scrolling = $(this).scrollTop();
 
-        if(scrolling > 50){
+        if (scrolling > 50) {
             $(".bottom-to-top").fadeIn(500);
-        }
-        else{
-            $(".bottom-to-top").fadeOut(500); 
+        } else {
+            $(".bottom-to-top").fadeOut(500);
         }
         // sticky menu //
-        if(scrolling > 100){
-            $("#nav").addClass("sticky-bg");
-        }
-        else{
-            $("#nav").removeClass("sticky-bg");
-        }
-      });
-
-     $(".bottom-to-top").click(function(){
-        $('html, body').animate({
-            scrollTop:0
-        }, 2000);
-     });      
-    // PRE LOADER //
-
-    $(window).on("load", function(){
-        $("#loading").delay(500) .fadeOut(2000)
+     
     });
+
+    $(".bottom-to-top").click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000);
+    });
+
+    // PRE LOADER //
+    $(window).on("load", function () {
+        $("#loading").delay(500).fadeOut(2000)
+    });
+
     // mixitup
     var mixer = mixitup('.mix-img');
-   
+
     // LIGHTBOX //
     lightbox.option({
         'resizeDuration': 200,
         'wrapAround': true,
     });
-
-
 });
